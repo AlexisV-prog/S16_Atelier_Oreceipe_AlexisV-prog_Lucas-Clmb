0c563364bbb7f3ba9cc7ab3ac46cf492650b8daa
AlexisV-prog <alexisvilley@gmail.com>
2025-07-16 11:35:05 +0200

    recipes interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
+import { useState } from 'react';
+
+interface IRecipes {
+  id: number,
+  title: string,
+  slug: string,
+  thumbnail: string,
+  author: string,
+  difficulty: string,
+  description: string,
+  instructions: [
+    string
+  ],
+  ingredients: [
+    {
+      name: string,
+      id: number,
+      quantity: number,
+      unit: string
+    }
+  ]
+}
 
 function App() {
 
+  const [recipes, setRecipes] = useState<IRecipes[]>([]);
 
   return ()
 }
